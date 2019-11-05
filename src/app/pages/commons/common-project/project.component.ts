92554c14b7a45eb2ea53d93f14e42e1d13f65c21
Ling <ling@pop-os.localdomain>
2019-11-05 16:42:54 +0100

    Mise en place de l'arrow dans zippy, page projects

--- src/app/pages/commons/common-project/project.component.ts
+++ src/app/pages/commons/common-project/project.component.ts
@@ -24,7 +24,7 @@ export class ProjectComponent implements OnInit {
       .getProjectById(this.project_id)
       .subscribe((data: Project) => {
         this.project = data['projects'];
-        //console.log(this.project);
+        console.log(this.project);
       })
   }
 
