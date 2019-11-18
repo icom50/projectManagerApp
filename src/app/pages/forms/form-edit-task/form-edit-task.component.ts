7b946483a771ea5bdee924a2b18002e5974a6513
Gabriel Dumortier <gabriel.dumortier@gmail.com>
2019-11-18 13:53:16 +0100

    integrate edit task

--- src/app/pages/forms/form-edit-task/form-edit-task.component.ts
+++ src/app/pages/forms/form-edit-task/form-edit-task.component.ts
@@ -95,7 +95,7 @@ export class FormEditTaskComponent implements OnInit {
       //console.log(this.project.users);
       for (let i = 0; i < this.project.users.length; i++) {
         this._dataService.getUserById(this.project.users[i]._id).subscribe((data: User) => {
-          // console.log(data);
+          console.log(data);
           this.emails.push(data['users'].email);
           console.log(this.emails);
         });
@@ -120,13 +120,16 @@ export class FormEditTaskComponent implements OnInit {
         //get his data by id
         this._dataService.getUserById(el.user_id).subscribe(data => {
 
-          //push the data in other temp table
-          this.memberAssignedAll.push(`${data['users'].firstname} ${data['users'].lastname}`);
-          
+          if(data['users'].firstname != '' || data['users'].lastname != '') {
+            //push the data in other temp table
+            this.memberAssignedAll.push(`${data['users'].firstname} ${data['users'].lastname}`);
+          } else {
+            this.memberAssignedAll.push(data['users'].email.split('@')[0]);
+          }
         });
         return this.memberAssignedAll;
       });
-      
+
       //otherTemp is used to get value outside dataservice
       this.otherTemp = names[0]; 
     })
